0284db8ab3bc182d401a67767b5d0464c32de316
Daniel015DJ <92205666+Daniel015DJ@users.noreply.github.com>
2021-10-09 11:07:03 +0200

    Add files via upload

---
+++ wb.js
@@ -0,0 +1,8 @@
+function hide(){
+    document.getElementById("left").style.display="none";
+    document.getElementById("hide").innerHTML= 'Show';
+  }
+function show(){
+    document.getElementById("left").style.display="block";
+    document.getElementById("hide").innerHTML='Hide';
+  }
